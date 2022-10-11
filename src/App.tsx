ed542ad2081983ae44259cd29fd1bef3781ffa2e
Vasil Yoshev <vasily@axilion.com>
2022-10-11 18:16:16 +0300

    Implement basic layout of the main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,46 @@
-import { Paper } from '@mui/material';
+import { useState } from 'react';
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from '@mui/material';
+import { useDebounce } from 'hooks';
 import styles from './App.module.scss';
 
-export const App = () => (
-  <>
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const debouncedSearchQuery = useDebounce(searchQuery, 500);
+
+  return (
     <Paper classes={{ root: styles.paperRoot }}>
-      Search
+      <TextField
+        value={searchQuery}
+        onChange={(event) => setSearchQuery(event.target.value)}
+      />
+      <span className={styles.selectedPairTitle}>
+        Selected pair: {debouncedSearchQuery}
+      </span>
+      <TableContainer component={Paper} className={styles.tableContainer}>
+        <Table>
+          <TableHead>
+            <TableRow className={styles.headTableRow}>
+              <TableCell>Provider</TableCell>
+              <TableCell>Price</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Binance</TableCell>
+              <TableCell>200</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Paper>
-  </>
-);
+  );
+};
